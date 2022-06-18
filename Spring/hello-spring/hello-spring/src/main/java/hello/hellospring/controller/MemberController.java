ac81242d1f363f3c4e72476550dfaee299e375fa
MHLEE12 <coolmh12@gmail.com>
2022-06-18 23:37:03 +0900

    스프링입문_웹 MVC 개발
    회원 웹 기능 등록 및 조회 추가

--- Spring/hello-spring/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ Spring/hello-spring/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,15 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -13,4 +20,30 @@ public class MemberController {
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping ("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+//        System.out.println("member = " + member.getName()); //이렇게 하면 입력된 이름을 확인할 수 있다.
+
+        memberService.join(member);
+
+        return "redirect:/"; // 회원가입 끝냈으니 홈화면으로 보내는 것
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+
+
 }
