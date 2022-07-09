c92ea43bda9539070b8deb9edfe3c553261e8cd1
MHLEE12 <coolmh12@gmail.com>
2022-07-09 22:24:11 +0900

    핵심원리기본_이해2_객체지향원리적용
    AppConfig 리팩터링

--- Spring_core_1/core/src/main/java/hello/core/AppConfig.java
+++ Spring_core_1/core/src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -10,11 +12,30 @@ import hello.core.order.OrderServiceImpl;
 public class AppConfig {
     //Application 전체를 설정하고 구성하는 클래스 - ex)공연 기획자
 
+//    public MemberService memberService() {
+//        return new MemberServiceImpl(new MemoryMemberRepository());
+//    }
+//    public OrderService orderService() {
+//        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+//    }
+
+    // 위의 것을 밑에 것으로 리팩토링! - 역할과 구현을 잘 보이게 함
+    // 중복되는 new MemoryMemberRepository() 를 제거해서 다른 구현체로 변경할 때 한 부분만 변경해도 되도록 했다.
+    
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
+    }
+
 }
