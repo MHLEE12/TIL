e487458d72442f5ad3dcafcd1faaab0e91c3ba5e
MHLEE12 <coolmh12@gmail.com>
2022-07-09 16:56:55 +0900

    핵심원리기본_이해1_예제만들기
    회원 도메인 실행과 테스트

---
+++ Spring_core_1/core/src/main/java/hello/core/MemberApp.java
@@ -0,0 +1,19 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        // id는 1인데 Long 타입이므로 L을 붙여준 것. L 빠지면 오류난다.
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("find Member = " + findMember.getName());
+    }
+}
