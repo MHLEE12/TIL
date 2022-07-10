7107595c4f0e8e8bfe32b21a88080913758c6910
MHLEE12 <coolmh12@gmail.com>
2022-07-10 10:08:25 +0900

    핵심원리기본_이해2_객체지향원리적용
    새로운 구조와 할인 정책 적용

--- Spring_core_1/core/src/main/java/hello/core/AppConfig.java
+++ Spring_core_1/core/src/main/java/hello/core/AppConfig.java
@@ -2,6 +2,7 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -23,19 +24,23 @@ public class AppConfig {
     // 중복되는 new MemoryMemberRepository() 를 제거해서 다른 구현체로 변경할 때 한 부분만 변경해도 되도록 했다.
     
     public MemberService memberService() {
+
         return new MemberServiceImpl(memberRepository());
     }
 
     private MemberRepository memberRepository() {
+
         return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
+
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+
+        return new RateDiscountPolicy();
     }
 
 }
