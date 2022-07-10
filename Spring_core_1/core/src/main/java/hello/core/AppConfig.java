6d1c5a01bfebc9b065329652525f9329338272f7
MHLEE12 <coolmh12@gmail.com>
2022-07-10 22:29:28 +0900

    핵심원리기본_이해2_객체지향원리적용
    스프링으로 전환하기

--- Spring_core_1/core/src/main/java/hello/core/AppConfig.java
+++ Spring_core_1/core/src/main/java/hello/core/AppConfig.java
@@ -9,7 +9,10 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
     //Application 전체를 설정하고 구성하는 클래스 - ex)공연 기획자
 
@@ -22,22 +25,26 @@ public class AppConfig {
 
     // 위의 것을 밑에 것으로 리팩토링! - 역할과 구현을 잘 보이게 함
     // 중복되는 new MemoryMemberRepository() 를 제거해서 다른 구현체로 변경할 때 한 부분만 변경해도 되도록 했다.
-    
+
+    @Bean
     public MemberService memberService() {
 
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
 
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
 
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
 
         return new RateDiscountPolicy();
--- Spring_core_1/core/src/main/java/hello/core/MemberApp.java
+++ Spring_core_1/core/src/main/java/hello/core/MemberApp.java
@@ -3,15 +3,23 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 
 public class MemberApp {
     public static void main(String[] args) {
 
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
 
 //        MemberService memberService = new MemberServiceImpl();
+
+        //이것이 컨테이너로 보면 됨. 객체들을 관리
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        //(메서드 이름, 타입)
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         // id는 1인데 Long 타입이므로 L을 붙여준 것. L 빠지면 오류난다.
         memberService.join(member);
