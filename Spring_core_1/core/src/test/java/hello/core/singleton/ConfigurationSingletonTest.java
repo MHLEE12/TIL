d9ef600b7d7184dfd1dd249b4d00af8d5d3b8e29
MHLEE12 <coolmh12@gmail.com>
2022-07-16 23:03:43 +0900

    핵심원리기본_싱글톤컨테이너
    @Configuration과 바이트코드 조작의 마법

--- Spring_core_1/core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ Spring_core_1/core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -33,7 +33,15 @@ public class ConfigurationSingletonTest {
 
     }
 
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
 
+        System.out.println("bean = " + bean.getClass());
+
+
+    }
 
 
 }
