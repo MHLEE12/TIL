de82b4949382b64667adcd3e10910fed2920b72c
MHLEE12 <coolmh12@gmail.com>
2022-09-01 23:41:36 +0900

    MVCv1-springMVC구조이해
    viewResolver

--- Spring_MVC_1/servlet/servlet/src/main/java/hello/servlet/ServletApplication.java
+++ Spring_MVC_1/servlet/servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -3,6 +3,9 @@ package hello.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @ServletComponentScan // 서블릿 자동 등록
 @SpringBootApplication
@@ -12,4 +15,9 @@ public class ServletApplication {
 		SpringApplication.run(ServletApplication.class, args);
 	}
 
+	// spring boot에서 application.properties에 설정한 prefix, suffix 정보를 보고 자동으로 밑의 코드처럼 작동하게 해줌
+//	@Bean
+//	ViewResolver internalResourceViewResolver() {
+//		return new InternalResourceViewResolver("/WEB-INF/views/", ".jsp");
+//	}
 }
