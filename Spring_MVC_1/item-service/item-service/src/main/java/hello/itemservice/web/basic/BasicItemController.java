8cc6305b0ca31dc33f9eae047cf65eec5977e450
MHLEE12 <coolmh12@gmail.com>
2022-09-06 19:21:38 +0900

    MVCv1-웹페이지 만들기
    상품 등록 폼

--- Spring_MVC_1/item-service/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ Spring_MVC_1/item-service/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.PostConstruct;
@@ -38,6 +39,16 @@ public class BasicItemController {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
         return "basic/item";
+    }
+
+    @GetMapping("/add") // data를 저장하거나 하는 것이 아닌 폼을 보여주는 것이니 @GetMapping사용
+    public String addForm() {
+        return "basic/addForm";
+    }
+
+    @PostMapping("/add")
+    public String save() {
+        return "basic/addForm";
     }
 
 
