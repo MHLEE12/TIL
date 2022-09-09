02e5f4989a9d6049774a226534a52c690e3984d1
MHLEE12 <coolmh12@gmail.com>
2022-09-09 23:03:17 +0900

    SpringBoot&JPA_prac1 - 주문 도메인 개발
    주문, 주문상품 엔티티 개발

--- Spring_JPA_prac1/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ Spring_JPA_prac1/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -49,6 +49,52 @@ public class Order {
         delivery.setOrder(this);
     }
 
+    // == 생성 메서드 == //
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        // 주문 생성을 여기에 응집
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for(OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER); // 처음 상태를 ORDER로 강제로 넣음
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    //== 비즈니스 로직 ==//
+    /**
+     * 주문 취소
+     */
+    public void cancel() {
+        if(delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+        this.setStatus(OrderStatus.CANCEL);
+        for(OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    //== 조회 로직 ==//
+    /**
+     * 전체 주문 가격 조회
+     */
+    public int getTotalPrice() {
+
+
+//        int totalPrice = 0;
+//        for (OrderItem orderItem : orderItems) {
+//            totalPrice += orderItem.getTotalPrice();
+//        }
+//        return totalPrice;
+
+        // 위의 코드와 같은 것
+        return orderItems.stream()
+                .mapToInt(OrderItem::getTotalPrice)
+                .sum();
+    }
 
 
 }
