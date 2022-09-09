9f1f562e9ec4930e55f121731396405c6ef0f5fb
MHLEE12 <coolmh12@gmail.com>
2022-09-09 10:38:57 +0900

    SpringBoot&JPA_prac1 - 도메인 분석 설계
    엔티티 클래스 개발2

--- Spring_JPA_prac1/jpashop/src/main/java/jpabook/jpashop/MemberRepository.java
+++ Spring_JPA_prac1/jpashop/src/main/java/jpabook/jpashop/MemberRepository.java
@@ -8,17 +8,17 @@ import javax.persistence.PersistenceContext;
 @Repository
 public class MemberRepository {
 
-    @PersistenceContext
-    EntityManager em; // spring boot가 자동으로 생성해줌
-
-    public Long save(Member member) {
-        em.persist(member);
-        return member.getId();
-    }
-
-    public Member find(Long id) {
-        return em.find(Member.class, id);
-    }
+//    @PersistenceContext
+//    EntityManager em; // spring boot가 자동으로 생성해줌
+//
+//    public Long save(Member member) {
+//        em.persist(member);
+//        return member.getId();
+//    }
+//
+//    public Member find(Long id) {
+//        return em.find(Member.class, id);
+//    }
 
 
 }
--- Spring_JPA_prac1/jpashop/src/main/java/jpabook/jpashop/domain/Address.java
+++ Spring_JPA_prac1/jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -13,4 +13,13 @@ public class Address {
     private String street;
     private String zipcode;
 
+    protected Address() {
+    }
+
+    // 생성자에서 값을 모두 초기화해서 변경 불가능한 클래스를 만듦.
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
