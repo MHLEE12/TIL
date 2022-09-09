c4b7afcc6f59336b925cd1370d67ac2368b2012d
MHLEE12 <coolmh12@gmail.com>
2022-09-09 22:11:18 +0900

    SpringBoot&JPA_prac1 - 상품 도메인 개발
    상품 엔티티 개발(비즈니스 로직 추가)

--- Spring_JPA_prac1/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ Spring_JPA_prac1/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -25,5 +26,25 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    // ==비즈니스 로직== //
+    // 객체지향적으로 보면 데이터를 가진 쪽에 비스니스 로직이 있는 것이 응집력이 높아져서 좋다.
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 
 }
