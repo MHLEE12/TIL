b356211eb82b51d18bab0266f8f95d57e3fc80bf
MHLEE12 <coolmh12@gmail.com>
2022-09-11 15:46:16 +0900

    SpringBoot&JPA_prac1 - 웹 계층 개발
    홈 화면과 레이아웃

---
+++ Spring_JPA_prac1/jpashop/src/main/java/jpabook/jpashop/controller/HomeController.java
@@ -0,0 +1,22 @@
+package jpabook.jpashop.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+public class HomeController {
+
+//    Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("/") // 제일 처음 화면
+    public String home() {
+        log.info("home controller");
+        return "home"; // home.html파일을 찾아감
+    }
+
+
+}
