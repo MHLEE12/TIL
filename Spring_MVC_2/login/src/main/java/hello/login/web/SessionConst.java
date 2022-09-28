0303c1ee96a5e81756b0435aa2b9ef3fab9f916f
MHLEE12 <coolmh12@gmail.com>
2022-09-28 22:09:53 +0900

    MVCv2 - 로그인 처리1 : 쿠키, 세션
    로그인 처리하기 - 서블릿 HTTP 세션1

---
+++ Spring_MVC_2/login/src/main/java/hello/login/web/SessionConst.java
@@ -0,0 +1,6 @@
+package hello.login.web;
+
+public abstract class SessionConst {
+    // 객체 생성할 의도가 아닌 상수로만 쓸것이므로 abstract나 interface 사용하는게 낫다.
+    public static final String LOGIN_MEMBER = "loginMember";
+}
