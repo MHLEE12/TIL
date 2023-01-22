6610fff4fa09758eca01b10cb1095a62abc8ef51
MHLEE12 <coolmh12@gmail.com>
2023-01-22 22:26:02 +0900

    study: 토비의 스프링부트 시작
    토비의 스프링부트 시작

---
+++ Spring/Toby's_SpringBoot/helloboot/src/test/java/mhspring/helloboot/HellobootApplicationTests.java
@@ -0,0 +1,13 @@
+package mhspring.helloboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellobootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
