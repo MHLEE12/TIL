21c45ee006a0545f4881d78be6e0a02fcf91f344
MHLEE12 <coolmh12@gmail.com>
2023-05-29 22:44:40 +0900

    Study: 스프링핵심원리 기본편 복습

--- Spring/Spring_core_1/core/src/main/java/hello/core/member/MemoryMemberRepository.java
+++ Spring/Spring_core_1/core/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -9,14 +9,17 @@ import java.util.Map;
 public class MemoryMemberRepository implements MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>();
+    // 실무에서는 동시성 이슈 때문에 ConcurrentHashMap 사용
 
     @Override
     public void save(Member member) {
+
         store.put(member.getId(), member);
     }
 
     @Override
     public Member findById(Long memberId) {
+
         return store.get(memberId);
     }
 }
