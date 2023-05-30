632b783a0df7245002ab29cc55118c26ce8fc575
MHLEE12 <coolmh12@gmail.com>
2023-05-30 23:06:40 +0900

    Study: ticketSeller class 개선

--- Book/Object/ObjectStudy/src/chapter01/Theater.java
+++ Book/Object/ObjectStudy/src/chapter01/Theater.java
@@ -10,19 +10,23 @@ public class Theater {
 	
 	public void enter(Audience audience) {
 		
-		if(audience.getBag().hasInvitation()) {
-			
-			Ticket ticket = ticketSeller.getTicketOffice().getTicket();
-			audience.getBag().setTicket(ticket);
-			
-		} else {
-			
-			Ticket ticket = ticketSeller.getTicketOffice().getTicket();
-			audience.getBag().minusAmount(ticket.getFee());
-			ticketSeller.getTicketOffice().plusAmount(ticket.getFee());
-			audience.getBag().setTicket(ticket);
-			
-		}
+		// 문제있던 기존 코드 -> 자율성을 높이기 위해 TicketSeller 내부로 숨기기
+//		if(audience.getBag().hasInvitation()) {
+//			
+//			Ticket ticket = ticketSeller.getTicketOffice().getTicket();
+//			audience.getBag().setTicket(ticket);
+//			
+//		} else {
+//			
+//			Ticket ticket = ticketSeller.getTicketOffice().getTicket();
+//			audience.getBag().minusAmount(ticket.getFee());
+//			ticketSeller.getTicketOffice().plusAmount(ticket.getFee());
+//			audience.getBag().setTicket(ticket);
+//			
+//		}
+		
+		// 개선
+		ticketSeller.sellTo(audience);
 	}
 	
 	
--- Book/Object/ObjectStudy/src/chapter01/TicketSeller.java
+++ Book/Object/ObjectStudy/src/chapter01/TicketSeller.java
@@ -8,8 +8,28 @@ public class TicketSeller {
 		this.ticketOffice = ticketOffice;
 	}
 
-	public TicketOffice getTicketOffice() {
-		return ticketOffice;
+	// ticketSeller의 자율성을 높이기 위해 삭제됨
+//	public TicketOffice getTicketOffice() {
+//		return ticketOffice;
+//	}
+	
+	// 자율성을 높히기 위해 Theater에 있던 코드를 옮김
+	public void sellTo(Audience audience) {
+		
+		if(audience.getBag().hasInvitation()) {
+			
+			Ticket ticket = ticketOffice.getTicket();
+			audience.getBag().setTicket(ticket);
+			
+		} else {
+			
+			Ticket ticket = ticketOffice.getTicket();
+			audience.getBag().minusAmount(ticket.getFee());
+			ticketOffice.plusAmount(ticket.getFee());
+			audience.getBag().setTicket(ticket);
+			
+		}
+		
 	}
 
 }
