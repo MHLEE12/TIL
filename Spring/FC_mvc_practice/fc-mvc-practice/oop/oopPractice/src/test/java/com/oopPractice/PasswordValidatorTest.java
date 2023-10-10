fb658a8ca47c57727daf1d3139647ee06ac9cf21
MHLEE12 <coolmh12@gmail.com>
2023-10-10 22:30:12 +0900

    Test: 경계값 테스트

--- Spring/FC_mvc_practice/fc-mvc-practice/oop/oopPractice/src/test/java/com/oopPractice/PasswordValidatorTest.java
+++ Spring/FC_mvc_practice/fc-mvc-practice/oop/oopPractice/src/test/java/com/oopPractice/PasswordValidatorTest.java
@@ -2,6 +2,8 @@ package com.oopPractice;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
 
@@ -16,8 +18,20 @@ public class PasswordValidatorTest {
 
     @DisplayName("비밀번호가 최소 8자 이상, 12자 이하면 예외가 발생하지 않는다.")
     @Test
-    void validatorPasswordTest() {
+    void validatePasswordTest() {
         assertThatCode(() -> PasswordValidator.validate("serverwizard"))
                 .doesNotThrowAnyException();
     }
+
+    @DisplayName("비밀번호가 8자 미만 12자 초과하는 경우 IllegalArgumentException 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"aabbcce", "aabbccddeeffg"})
+    void validatePasswordTest2(String password) {
+        // 위의 ValueSource에 써둔 값이 파라미터로 들어오게 됨
+        assertThatCode(() -> PasswordValidator.validate(password))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("비밀번호는 최소 8자 이상 12자 이하여야 한다.");
+    }
+
+
 }
