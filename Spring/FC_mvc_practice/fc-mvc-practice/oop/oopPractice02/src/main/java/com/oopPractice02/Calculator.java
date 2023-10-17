51e7ac2b593da2fa06809da0b06735a72aff8492
MHLEE12 <coolmh12@gmail.com>
2023-10-17 23:08:21 +0900

    Study: oop실습01 - 사칙연산 계산기1
    덧셈 연산 기능 test

---
+++ Spring/FC_mvc_practice/fc-mvc-practice/oop/oopPractice02/src/main/java/com/oopPractice02/Calculator.java
@@ -0,0 +1,10 @@
+package com.oopPractice02;
+
+public class Calculator {
+
+
+    public static int calculate(int operand1, String operator, int operand2) {
+
+        return operand1 + operand2;
+    }
+}
