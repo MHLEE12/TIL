55c13fc9210fa8fdddbbe4eeb601488981dd25db
MHLEE12 <coolmh12@gmail.com>
2023-10-19 22:40:22 +0900

    refactor: 덧셈 연산 기능

--- Spring/FC_mvc_practice/fc-mvc-practice/oop/oopPractice02/src/main/java/com/oopPractice02/Calculator.java
+++ Spring/FC_mvc_practice/fc-mvc-practice/oop/oopPractice02/src/main/java/com/oopPractice02/Calculator.java
@@ -4,7 +4,10 @@ public class Calculator {
 
 
     public static int calculate(int operand1, String operator, int operand2) {
+        if ("+".equals(operator)) {
+            return operand1 + operand2;
+        }
 
-        return operand1 + operand2;
+        return 0;
     }
 }
