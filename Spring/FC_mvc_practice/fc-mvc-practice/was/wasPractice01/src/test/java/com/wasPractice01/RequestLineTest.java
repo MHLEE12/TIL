f4636303c3cf5ff95c0dbff0e28b9dde0f0c062f
MHLEE12 <coolmh12@gmail.com>
2023-11-04 21:32:07 +0900

    Test: RequestLine 테스트

---
+++ Spring/FC_mvc_practice/fc-mvc-practice/was/wasPractice01/src/test/java/com/wasPractice01/RequestLineTest.java
@@ -0,0 +1,15 @@
+package com.wasPractice01;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+public class RequestLineTest {
+
+    @Test
+    void create() {
+        RequestLine requestLine = new RequestLine("GET /calculate?operand1=10&operator=*&operand2=33");
+
+        assertThat(requestLine).isNotNull();
+    }
+}
