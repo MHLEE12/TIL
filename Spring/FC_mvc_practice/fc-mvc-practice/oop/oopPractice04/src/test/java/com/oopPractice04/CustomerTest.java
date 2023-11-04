379871e57fb5029607c971f4fa839d123e633911
MHLEE12 <coolmh12@gmail.com>
2023-11-04 13:41:09 +0900

    test: 고객 테스트

--- Spring/FC_mvc_practice/fc-mvc-practice/oop/oopPractice04/src/test/java/com/oopPractice04/CustomerTest.java
+++ Spring/FC_mvc_practice/fc-mvc-practice/oop/oopPractice04/src/test/java/com/oopPractice04/CustomerTest.java
@@ -1,6 +1,11 @@
 package com.oopPractice04;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
 
 /**
  * 음식점에서 음식 주문하는 과정 구현
@@ -14,8 +19,15 @@ import org.junit.jupiter.api.Test;
  */
 public class CustomerTest {
 
+    @DisplayName("메뉴 이름에 해당하는 요리를 주문한다.")
     @Test
-    void name() {
+    void orderTest() {
+        Customer customer = new Customer();
+        Menu menu = new Menu(List.of(new MenuItem("돈까스", 5000), new MenuItem("커리", 7000)));
+        Cooking cooking = new Cooking();
 
+
+        assertThatCode(() -> customer.order("커리", menu, cooking))
+                .doesNotThrowAnyException();
     }
 }
