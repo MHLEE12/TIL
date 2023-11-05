34fd8a070107eae101018354dd20247f3c4c6f87
MHLEE12 <coolmh12@gmail.com>
2023-11-05 20:54:55 +0900

    Study: 계산기 웹 프로그램 구현
    Step3 - Thread Pool을 적용해 안정적인 서비스가 가능하도록 한다.

--- Spring/FC_mvc_practice/fc-mvc-practice/was/wasPractice01/src/main/java/com/wasPractice01/CustomWebApplicationServer.java
+++ Spring/FC_mvc_practice/fc-mvc-practice/was/wasPractice01/src/main/java/com/wasPractice01/CustomWebApplicationServer.java
@@ -8,10 +8,14 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class CustomWebApplicationServer {
 
     private final int port;
+
+    private final ExecutorService executorService = Executors.newFixedThreadPool(10);
 
     private static final Logger logger = LoggerFactory.getLogger(CustomWebApplicationServer.class);
 
@@ -30,10 +34,9 @@ public class CustomWebApplicationServer {
                 logger.info("[CustomWebApplicationServer] Client connected!");
 
                 /**
-                 * Step2 - 사용자 요청이 들어올 때마다 Thread를 새로 생성해서 사용자 요청을 처리하도록 한다.
+                 * Step3 - Thread Pool을 적용해 안정적인 서비스가 가능하도록 한다.
                  */
-                // Thread는 생성될 때마다 독립적인 stack memory 공간을 할당받음.
-                new Thread(new ClientRequestHandler(clientSocket)).start();
+                executorService.execute(new ClientRequestHandler(clientSocket));
             }
         }
     }
