e5392ef339e8cb35eec424f65043413b01f45e34
MHLEE12 <coolmh12@gmail.com>
2023-11-25 22:06:52 +0900

    Study: React study - 5. event
    5. event

--- Front/React/react-app/src/App.js
+++ Front/React/react-app/src/App.js
@@ -4,14 +4,23 @@ function Header(props) {
   // console.log('props', props);
   // console.log('props2', props.title);
   return <header>
-  <h1><a href="/">{props.title}</a></h1>
+  <h1><a href="/" onClick={function(event){
+    event.preventDefault(); // 기본동작 방지
+    props.onChangeMode();
+  }}>{props.title}</a></h1>
 </header>
 }
+
 function Nav(props) {
   const lis = [];
   for(let i = 0; i < props.topics.length; i++) {
     let t = props.topics[i];
-    lis.push(<li key={t.id}><a href={'/read/' + t.id}>{t.title}</a></li>);
+    lis.push(<li key={t.id}>
+        <a id={t.id} href={'/read/' + t.id} onClick={event=>{
+          event.preventDefault();
+          props.onChangeMode(event.target.id);
+        }}>{t.title}</a>
+      </li>);
   }
   return <nav>
   <ol>
@@ -19,6 +28,7 @@ function Nav(props) {
   </ol>
 </nav>
 }
+
 function Article(props) {
   return <article>
   <h2>{props.title}</h2>
@@ -34,8 +44,12 @@ function App() {
   ]; 
   return (
     <div>
-      <Header title="Web"></Header>
-      <Nav topics={topics}></Nav>
+      <Header title="Web" onChangeMode={()=>{
+        alert('Header!');
+      }}></Header>
+      <Nav topics={topics} onChangeMode={(id)=>{
+        alert(id);
+      }}></Nav>
       <Article title="Welcome" body="Hello Web"></Article>
       {/* <Article title="Hello" body="Hello React"></Article> */}
     </div>
