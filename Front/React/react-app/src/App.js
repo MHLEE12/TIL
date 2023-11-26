c2e7b517039a52c28f8285a6cd08dc8856bd0a6c
MHLEE12 <coolmh12@gmail.com>
2023-11-26 22:01:43 +0900

    Study: React study - 6. state
    6. state

--- Front/React/react-app/src/App.js
+++ Front/React/react-app/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
 function Header(props) {
   // console.log('props', props);
   // console.log('props2', props.title);
@@ -18,7 +20,7 @@ function Nav(props) {
     lis.push(<li key={t.id}>
         <a id={t.id} href={'/read/' + t.id} onClick={event=>{
           event.preventDefault();
-          props.onChangeMode(event.target.id);
+          props.onChangeMode(Number(event.target.id));
         }}>{t.title}</a>
       </li>);
   }
@@ -37,21 +39,40 @@ function Article(props) {
 }
 
 function App() {
+  // const _mode = useState('WELCOME');
+  // const mode = _mode[0];
+  // const setMode  = _mode[1];
+  const [mode, setMode] = useState('WELCOME'); // 위의 3줄을 한 줄로 표현한 것
+  const [id, setId] = useState(null);
+
   const topics = [
     {id:1, title:'html', body:'html is ~~'},
     {id:2, title:'css', body:'css is ~~'},
     {id:3, title:'js', body:'js is ~~'}
   ]; 
+  let content = null;
+  if(mode === 'WELCOME') {
+    content = <Article title="Welcome" body="Hello Web"></Article>
+  } else if(mode === 'Read') {
+    let title, body = null;
+    for(let i = 0; i < topics.length; i++) {
+      if(topics[i].id === id) {
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = content = <Article title={title} body={body}></Article>
+  }
   return (
     <div>
       <Header title="Web" onChangeMode={()=>{
-        alert('Header!');
+        setMode('WELCOME'); // setMode가 실행되면 App function이 재실행 되어서 mode값이 변경됨
       }}></Header>
-      <Nav topics={topics} onChangeMode={(id)=>{
-        alert(id);
+      <Nav topics={topics} onChangeMode={(_id)=>{
+        setMode('Read');
+        setId(_id);
       }}></Nav>
-      <Article title="Welcome" body="Hello Web"></Article>
-      {/* <Article title="Hello" body="Hello React"></Article> */}
+      {content}
     </div>
   );
 }
