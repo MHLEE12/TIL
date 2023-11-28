31fc90feb0a580b1d085bf086868c025c61a0092
MHLEE12 <coolmh12@gmail.com>
2023-11-28 18:13:49 +0900

    Study: React study - 7. Create
    7. Create

--- Front/React/react-app/src/App.js
+++ Front/React/react-app/src/App.js
@@ -37,6 +37,21 @@ function Article(props) {
   {props.body}
 </article>
 }
+function Create(props) {
+  return <article>
+    <h2>Create</h2>
+    <form onSubmit={event=>{
+      event.preventDefault();
+      const title = event.target.title.value;
+      const body = event.target.body.value;
+      props.onCreate(title, body);
+    }}>
+      <p><input type="text" name="title" placeholder='title'/></p>
+      <p><textarea name="body" placeholder='body'></textarea></p>
+      <p><input type="submit" value="Create"></input></p>
+    </form>
+  </article>
+}
 
 function App() {
   // const _mode = useState('WELCOME');
@@ -44,16 +59,17 @@ function App() {
   // const setMode  = _mode[1];
   const [mode, setMode] = useState('WELCOME'); // 위의 3줄을 한 줄로 표현한 것
   const [id, setId] = useState(null);
-
-  const topics = [
+  
+  const [nextId, setNextId] = useState(4);
+  const [topics, setTopics] = useState([
     {id:1, title:'html', body:'html is ~~'},
     {id:2, title:'css', body:'css is ~~'},
     {id:3, title:'js', body:'js is ~~'}
-  ]; 
+  ]); 
   let content = null;
   if(mode === 'WELCOME') {
     content = <Article title="Welcome" body="Hello Web"></Article>
-  } else if(mode === 'Read') {
+  } else if(mode === 'READ') {
     let title, body = null;
     for(let i = 0; i < topics.length; i++) {
       if(topics[i].id === id) {
@@ -62,6 +78,17 @@ function App() {
       }
     }
     content = content = <Article title={title} body={body}></Article>
+  } else if(mode === 'CREATE') {
+    content = <Create onCreate={(_title, _body)=>{
+      const newTopic = {id: nextId, title: _title, body: _body};
+      // setTopics 실행시 기존 배열(객체 등)과 같은지 비교 후에 같으면 set이 실행하지 않고, 달라야 실행하므로 새로운 배열로 복제해서 set실행
+      const newTopics = [...topics];
+      newTopics.push(newTopic);
+      setTopics(newTopics);
+      setMode('READ');
+      setId(nextId);
+      setNextId(nextId + 1);
+    }}></Create>
   }
   return (
     <div>
@@ -69,10 +96,14 @@ function App() {
         setMode('WELCOME'); // setMode가 실행되면 App function이 재실행 되어서 mode값이 변경됨
       }}></Header>
       <Nav topics={topics} onChangeMode={(_id)=>{
-        setMode('Read');
+        setMode('READ');
         setId(_id);
       }}></Nav>
       {content}
+      <a href="/create" onClick={event=>{
+        event.preventDefault();
+        setMode('CREATE');
+      }}>Create</a>
     </div>
   );
 }
