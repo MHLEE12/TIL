45dd3ea5933816d1b9f280def85dc2d28327af59
MHLEE12 <coolmh12@gmail.com>
2023-11-29 23:17:12 +0900

    Study: node를 이용해서 Todo app 생성
    node를 이용해서 todo list를 작성하는 app을 만들어본다.

---
+++ NodeJS/node_todoapp/server.js
@@ -0,0 +1,8 @@
+// express라이브러리를 이용해서 server를 띄우기 위한 기본 세팅
+const express = require('express'); // 설치한 라이브러리 첨부를 요구
+const app = express(); // 첨부된 것으로 새로운 객체 생성
+
+// 서버를 엶. listen(포트번호, 서버 띄운 후 실행할 코드)
+app.listen(8081, function() {
+    console.log('listening on 8081');
+});
