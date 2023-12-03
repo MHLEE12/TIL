6289e1b619d3e9d0b7dab05945f4e35edcb40142
MHLEE12 <coolmh12@gmail.com>
2023-12-03 23:06:40 +0900

    Study: node - 라우터 요청

--- NodeJS/basic_node/app.js
+++ NodeJS/basic_node/app.js
@@ -23,6 +23,15 @@ const server = http.createServer((req, res) => {
 
     // process.exit(); // 이벤트 루프를 잘라서 프로그램을 종료 시킴
 
+    const url = req.url;
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My First Page</title></head>');
