2b9fd7d8b246ae4572fbc6d1db88d12b204b3aad
MHLEE12 <coolmh12@gmail.com>
2023-12-03 22:50:56 +0900

    Study: node - 요청 및 응답 전송
    node - 요청 및 응답 전송

--- NodeJS/basic_node/app.js
+++ NodeJS/basic_node/app.js
@@ -16,9 +16,19 @@ const http = require('http');
 
 // 서버 여는 방법 3. createServer 콜백 함수 사용
 const server = http.createServer((req, res) => {
-    console.log(req);
+    // 실행은 터미널에서 > node [파일명] (여기서는 node app.js 로 실행) 
+
+    // console.log(req);
+    // console.log(req.url, req.method, req.headers);
 
     // process.exit(); // 이벤트 루프를 잘라서 프로그램을 종료 시킴
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Hello from my Node.js Server!!</h1></body>');
+    res.write('</html>');
+    res.end(); // 응답을 끝냄. 이 뒤에 write를 작성하면 에러 발생
 });
 
 server.listen(8081);
