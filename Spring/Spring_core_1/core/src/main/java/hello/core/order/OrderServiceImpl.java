a6168bdde360fa6609848eb72c50ece8d359037b
MHLEE12 <coolmh12@gmail.com>
2023-12-05 11:24:34 +0900

    핵심원리기본-의존관계자동주입
    다양한 의존관계 주입 방법

--- Spring/Spring_core_1/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ Spring/Spring_core_1/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -22,6 +22,10 @@ public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
