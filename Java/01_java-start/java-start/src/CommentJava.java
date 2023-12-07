7a6cb6067440c756bbed7dd513e8bae108e917b3
MHLEE12 <coolmh12@gmail.com>
2023-12-07 19:36:41 +0900

    Study: Hello World - 주석(comment)

---
+++ Java/01_java-start/java-start/src/CommentJava.java
@@ -0,0 +1,12 @@
+public class CommentJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java 1"); // Hello Java1을 출력합니다.
+//        System.out.println("Hello Java 2");
+
+        /*
+        System.out.println("Hello Java 4");
+        System.out.println("Hello Java 3");
+        */
+    }
+}
