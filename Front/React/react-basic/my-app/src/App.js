7390b7a2add8a0e0033dacbf88b321f8f69f10ec
MHLEE12 <coolmh12@gmail.com>
2023-12-12 23:12:23 +0900

    Study: React.js 입문 - 2
    [한시간 만에 끝내는 React.js 입문] 유튜브 강의 수강
    https://www.youtube.com/watch?v=6GECT2Jrr_g&t=14s

--- Front/React/react-basic/my-app/src/App.js
+++ Front/React/react-basic/my-app/src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Counter from "./pages/Counter";
+import Input from "./pages/input";
 
 function App() {
   return (
@@ -9,11 +11,14 @@ function App() {
     // 이 안에서는 class가 아닌 className을 써야 한다.
     <div className="App">
       <nav>
-        <Link to="/">Home</Link> | <Link to="/about">About</Link> 
+        <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/counter">Counter</Link> |{" "}
+        <Link to="/input">Input</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/counter" element={<Counter />} />
+        <Route path="/input" element={<Input />} />
       </Routes>
     </div>
   );
