4db99c494917be106d4c1034ca97c65fb3f96ce8
MHLEE12 <coolmh12@gmail.com>
2023-12-16 18:22:21 +0900

    Study: 독립 실행형 서블릿 애플리케이션 - 프론트 컨트롤러로 전환

--- Spring/Toby's_SpringBoot/helloboot/src/main/java/mhspring/helloboot/HellobootApplication.java
+++ Spring/Toby's_SpringBoot/helloboot/src/main/java/mhspring/helloboot/HellobootApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -27,16 +28,23 @@ public class HellobootApplication {
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
             // 서블릿 컨테이너에 서블릿 추가
             // ServletContextInitializer 익명클래스를 람다식으로 바꿨음
-            servletContext.addServlet("hello", new HttpServlet() {
+            servletContext.addServlet("frontController", new HttpServlet() {
                 @Override
                 protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                    String name = req.getParameter("name");
+                    // 인증, 보안, 다국어, 공통 기능
+                    if(req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+                        String name = req.getParameter("name");
 
-                    resp.setStatus(HttpStatus.OK.value()); // 상태 코드 '200'
-                    resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE); // 헤더 설정
-                    resp.getWriter().println("Hello " + name); // 바디 작성
+                        resp.setStatus(HttpStatus.OK.value()); // 상태 코드 '200'
+                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE); // 헤더 설정
+                        resp.getWriter().println("Hello " + name); // 바디 작성
+                    } else if(req.getRequestURI().equals("/user")) {
+                        //
+                    } else {
+                        resp.setStatus(HttpStatus.NOT_FOUND.value()); // 상태 코드 '404'
+                    }
                 }
-            }).addMapping("/hello"); // 서블릿 컨테이너가 요청이 들어왔을때 어떤 서블릿과 연결할지 설정함(매핑)
+            }).addMapping("/*"); // 서블릿 컨테이너가 요청이 들어왔을때 어떤 서블릿과 연결할지 설정함(매핑)
         });
         webServer.start(); // 톰캣 서블릿 컨테이너 실행
     }
