244b7f29e2b84b3a3a738c48bc3d8f01825ed47c
MHLEE12 <coolmh12@gmail.com>
2023-12-19 17:44:17 +0900

    Study: 독립 실행형 서블릿 애플리케이션 - SpringBootApplication 1

--- Spring/Toby's_SpringBoot/helloboot/src/main/java/mhspring/helloboot/HellobootApplication.java
+++ Spring/Toby's_SpringBoot/helloboot/src/main/java/mhspring/helloboot/HellobootApplication.java
@@ -2,26 +2,12 @@ package mhspring.helloboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
-import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
-
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Configuration
 @ComponentScan
@@ -38,26 +24,8 @@ public class HellobootApplication {
     }
 
     public static void main(String[] args) {
-        // Spring Container
-        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
-            // 익명클래스 사용
-            @Override
-            protected void onRefresh() {
-                super.onRefresh();
-
-                ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
-                DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
-//                dispatcherServlet.setApplicationContext(this); 명시적으로 쓰지 않아도 됨
-
-                WebServer webServer = serverFactory.getWebServer(servletContext -> {
-                    servletContext.addServlet("dispatcherServlet", dispatcherServlet)
-                            .addMapping("/*"); // 서블릿 컨테이너가 요청이 들어왔을때 어떤 서블릿과 연결할지 설정함(매핑)
-                });
-                webServer.start(); // 톰캣 서블릿 컨테이너 실행
-            }
-        };
-        applicationContext.register(HellobootApplication.class); // 자바코드로 된 구성정보를 가진 클래스를 등록해줌
-        applicationContext.refresh(); // 컨테이너를 초기화하여 가지고 있는 bean object 생성
-
+        MySpringApplication.run(HellobootApplication.class, args);
     }
+
+
 }
