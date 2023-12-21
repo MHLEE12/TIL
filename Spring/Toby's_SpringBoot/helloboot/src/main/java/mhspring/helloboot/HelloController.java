a972095f4dd13b489561fd77587ee5686b16be6d
MHLEE12 <coolmh12@gmail.com>
2023-12-21 17:41:40 +0900

    Study: DI와 테스트, 디자인 패턴 - DI와 단위 테스트

--- Spring/Toby's_SpringBoot/helloboot/src/main/java/mhspring/helloboot/HelloController.java
+++ Spring/Toby's_SpringBoot/helloboot/src/main/java/mhspring/helloboot/HelloController.java
@@ -9,18 +9,17 @@ import java.util.Objects;
 @RestController
 public class HelloController {
     private final HelloService helloService;
-    private final ApplicationContext applicationContext;
 
-    public HelloController(HelloService helloService, ApplicationContext applicationContext) {
+    public HelloController(HelloService helloService) {
         this.helloService = helloService;
-        this.applicationContext = applicationContext;
     }
 
 //    @RequestMapping(value = "/hello", method = RequestMethod.GET) <- @GetMapping("/hello") 와 같은 의미
 //    @ResponseBody // 이 어노테이션을 쓰지 않으면 반환되는 string을 view name으로 해석함. 클래스 단위로 @RestController 를 사용하면 어노테이션 생략 가능
     @GetMapping("/hello")
     public String hello(String name) {
+        if (name == null || name.trim().length() == 0) throw new IllegalArgumentException();
 
-        return helloService.sayHello(Objects.requireNonNull(name)); // null이면 예외를 던짐. null이 아니면 정상적으로 작동.
+        return helloService.sayHello(name);
     }
 }
