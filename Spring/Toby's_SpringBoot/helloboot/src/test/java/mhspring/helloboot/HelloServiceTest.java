a4b9a353cea5186c92800afe4a1914c83ab1aad9
MHLEE12 <coolmh12@gmail.com>
2023-12-22 22:31:27 +0900

    Study: 자동 구성 기반 애플리케이션 - 메타 애노테이션과 합성 애노테이션 1

--- Spring/Toby's_SpringBoot/helloboot/src/test/java/mhspring/helloboot/HelloServiceTest.java
+++ Spring/Toby's_SpringBoot/helloboot/src/test/java/mhspring/helloboot/HelloServiceTest.java
@@ -3,12 +3,29 @@ package mhspring.helloboot;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@UnitTest
+@interface FastUnitTest {
+}
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.ANNOTATION_TYPE, ElementType.METHOD}) // ElementType.ANNOTATION_TYPE 다른 어노테이션의 메타에노테이션으로도 사용 가능함
+@Test
+@interface UnitTest {
+}
+
 class HelloServiceTest {
 
-    @Test
+    @FastUnitTest
     void simpleHelloService() {
         SimpleHelloService helloService = new SimpleHelloService();
         String ret = helloService.sayHello("Test");
