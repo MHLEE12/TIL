c012c091826427375565618d04a945f769f092df
MHLEE12 <coolmh12@gmail.com>
2023-12-22 22:37:48 +0900

    Study: DI와 테스트, 디자인 패턴 - 합성 애노테이션의 적용

--- Spring/Toby's_SpringBoot/helloboot/src/main/java/mhspring/helloboot/Config.java
+++ Spring/Toby's_SpringBoot/helloboot/src/main/java/mhspring/helloboot/Config.java
@@ -1,17 +1,20 @@
 package mhspring.helloboot;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
 
-@MySpringBootAnnotation
-public class HellobootApplication {
+@Configuration
+public class Config {
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        return new TomcatServletWebServerFactory(8081);
+    }
 
-    public static void main(String[] args) {
-        SpringApplication.run(HellobootApplication.class, args);
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
     }
 }
