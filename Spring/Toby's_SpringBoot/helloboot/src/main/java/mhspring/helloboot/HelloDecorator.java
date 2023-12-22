8712fa32c0010d25c83c8c8f1a70d5bdb14c70c9
MHLEE12 <coolmh12@gmail.com>
2023-12-22 22:18:54 +0900

    Study: DI와 테스트, 디자인 패턴 - DI를 이용한 Decorator, Proxy 패턴

---
+++ Spring/Toby's_SpringBoot/helloboot/src/main/java/mhspring/helloboot/HelloDecorator.java
@@ -0,0 +1,20 @@
+package mhspring.helloboot;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Service
+@Primary // 이 오브젝트가 우선적으로 빈 주입함
+public class HelloDecorator implements HelloService {
+
+    private final HelloService helloService;
+
+    public HelloDecorator(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
+    @Override
+    public String sayHello(String name) {
+        return "*" + helloService.sayHello(name) + "*";
+    }
+}
