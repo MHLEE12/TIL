10c138a28eace422e8ce4bf650b0e12add5a81fd
MHLEE12 <coolmh12@gmail.com>
2023-12-26 23:03:25 +0900

    Study: 외부 설정을 이용한 자동 구성 - 자동 구성에 Environment 프로퍼티 적용 1

--- Spring/Toby's_SpringBoot/helloboot/src/main/java/mhspring/helloboot/HellobootApplication.java
+++ Spring/Toby's_SpringBoot/helloboot/src/main/java/mhspring/helloboot/HellobootApplication.java
@@ -2,11 +2,24 @@ package mhspring.helloboot;
 
 import mhspring.config.EnableMyAutoConfiguration;
 import mhspring.config.MySpringBootApplication;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @MySpringBootApplication
 @EnableMyAutoConfiguration
 public class HellobootApplication {
+
+    @Bean
+    ApplicationRunner applicationRunner(Environment env) {
+        // System > Environment > Application
+        return args -> {
+            String name = env.getProperty("my.name");
+            System.out.println("my.name = " + name);
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(HellobootApplication.class, args);
