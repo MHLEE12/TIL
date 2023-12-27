e5e8baceabeabb023415ce38d5c4fe7df83c9f7b
MHLEE12 <coolmh12@gmail.com>
2023-12-27 22:18:02 +0900

    Study: 외부 설정을 이용한 자동 구성 - @Value와 PropertySourcePlaceholderConfigurer

---
+++ Spring/Toby's_SpringBoot/helloboot/src/main/java/mhspring/config/autoconfig/PropertyPlaceholderConfig.java
@@ -0,0 +1,13 @@
+package mhspring.config.autoconfig;
+
+import mhspring.config.MyAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@MyAutoConfiguration
+public class PropertyPlaceholderConfig {
+    @Bean
+    PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+}
--- Spring/Toby's_SpringBoot/helloboot/src/main/java/mhspring/config/autoconfig/TomcatWebServerConfig.java
+++ Spring/Toby's_SpringBoot/helloboot/src/main/java/mhspring/config/autoconfig/TomcatWebServerConfig.java
@@ -2,6 +2,7 @@ package mhspring.config.autoconfig;
 
 import mhspring.config.ConditionalMyOnClass;
 import mhspring.config.MyAutoConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
@@ -11,13 +12,14 @@ import org.springframework.core.env.Environment;
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
+    @Value("${contextPath}")
+    String contextPath;
 
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean // 리턴 타입의 빈이 등록되어 있는지 확인하고 없다면 이것으로 빈 등록 한다.
-    public ServletWebServerFactory servletWebServerFactory(Environment env) {
+    public ServletWebServerFactory servletWebServerFactory() {
         TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory(8086);
-//        factory.setContextPath("/app"); // http://localhost:8086/app/hello?name=Spring
-        factory.setContextPath(env.getProperty("contextPath"));
+        factory.setContextPath(this.contextPath);
         return factory;
     }
 }
