ebf6175b5655b51b28fd46beb00dc9a5d7402ab3
MHLEE12 <coolmh12@gmail.com>
2023-12-27 22:23:49 +0900

    Study: 외부 설정을 이용한 자동 구성 - 프로퍼티 클래스의 분리 1

--- Spring/Toby's_SpringBoot/helloboot/src/main/java/mhspring/config/autoconfig/TomcatWebServerConfig.java
+++ Spring/Toby's_SpringBoot/helloboot/src/main/java/mhspring/config/autoconfig/TomcatWebServerConfig.java
@@ -12,14 +12,20 @@ import org.springframework.core.env.Environment;
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
-    @Value("${contextPath}")
+    @Value("${contextPath:}")
     String contextPath;
+
+    @Value("${port:8081}") // : 뒤에는 디폴트 값이 들어감. property값이 없는 경우 :의 오른쪽 값이 디폴트로 들어감.
+    int port;
 
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean // 리턴 타입의 빈이 등록되어 있는지 확인하고 없다면 이것으로 빈 등록 한다.
     public ServletWebServerFactory servletWebServerFactory() {
         TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory(8086);
+
         factory.setContextPath(this.contextPath);
+        factory.setPort(port);
+
         return factory;
     }
 }
