8866299034bf01d15fd40a03412e2cc0d650af43
MHLEE12 <coolmh12@gmail.com>
2023-12-29 22:01:08 +0900

    Study: Spring JDBC 자동 구성 개발 - JdbcTemplate와 트랜젝션 매니저 구성

---
+++ Spring/Toby's_SpringBoot/helloboot/src/test/java/mhspring/helloboot/JdbcTemplateTest.java
@@ -0,0 +1,40 @@
+package mhspring.helloboot;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import static org.assertj.core.api.Assertions.*;
+
+@HellobootTest
+public class JdbcTemplateTest {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    void init() {
+        jdbcTemplate.execute("create table if not exists hello(name varchar(50) primary key, count int)");
+    }
+
+    @Test
+    void insertAndQuery() {
+        jdbcTemplate.update("insert into hello values(?, ?)" , "mh", 3);
+        jdbcTemplate.update("insert into hello values(?, ?)" , "Spring", 1);
+
+        Long count = jdbcTemplate.queryForObject("select count(*) from hello", Long.class);
+        assertThat(count).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("트랜젝션 작동 테스트")
+    void insertAndQuery2() {
+        jdbcTemplate.update("insert into hello values(?, ?)" , "mh", 3);
+        jdbcTemplate.update("insert into hello values(?, ?)" , "Spring", 1);
+
+        Long count = jdbcTemplate.queryForObject("select count(*) from hello", Long.class);
+        assertThat(count).isEqualTo(2);
+    }
+}
