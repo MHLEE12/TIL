ecc3564e11deffebef6f37bdac31bed66a69656a
MHLEE12 <coolmh12@gmail.com>
2023-12-30 22:44:25 +0900

    Study: Spring JDBC 자동 구성 개발 - 리포지토리를 사용하는 HelloService

--- Spring/Toby's_SpringBoot/helloboot/src/main/java/mhspring/helloboot/HelloService.java
+++ Spring/Toby's_SpringBoot/helloboot/src/main/java/mhspring/helloboot/HelloService.java
@@ -5,4 +5,8 @@ import org.springframework.stereotype.Service;
 @Service
 public interface HelloService {
     String sayHello(String name);
+
+    default int countOf(String name){
+        return 0;
+    }
 }
