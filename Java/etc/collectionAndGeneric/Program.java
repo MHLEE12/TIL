3f86b428d2ae197fe5a268d3aa4fa797408e4651
MHLEE12 <coolmh12@gmail.com>
2024-01-03 22:34:28 +0900

    Study: 자바 컬렉션과 제네릭 - Set/List/Map 컬렉션

--- Java/etc/collectionAndGeneric/Program.java
+++ Java/etc/collectionAndGeneric/Program.java
@@ -1,14 +1,38 @@
+import java.util.*;
+
 public class Program {
     public static void main(String[] args) {
-        GList<Integer> list = new GList<>();
+        // 가변길이 배열
+        List<Integer> list = new ArrayList<>();
         list.add(3);
         list.add(5);
         list.add(6);
         list.add(7);
+        list.add(7);
+        list.add(7);
+        System.out.println("List => " + list.get(5));
+        System.out.println("List size => " + list.size());
 
-        for(int i = 0; i < list.size(); i++) {
-            System.out.println("list" + "[" + i + "] = " + list.get(i));
-        }
+        // List는 식별자가 있으나, Set은 식별자가 없음
+        // set은 값 자체가 키이므로 중복이 될 수 없다.
+        // 중복 제거용
+        Set<Object> set = new HashSet<>();
+        set.add(3);
+        set.add(5);
+        set.add(6);
+        set.add(7);
+        set.add(7);
+        set.add(7);
+        System.out.println(set.add(7)); // 기존에 있던 값이면 false 출력
+        System.out.println("Set => " + set.toString());
+        System.out.println("Set size => " + set.size());
 
+        // 클래스로 정의하지 않고, 속성과 값으로 구분된 데이터 식별의 개체를 필요로 할 때 사용
+        Map<String, Object> map = new HashMap<>();
+        map.put("id", 3);
+        map.put("title", "Hello");
+        map.put("hit", 12);
+
+        System.out.println(map.get("title"));
     }
 }
