b5a1f25e6a4fe242de810c0b7b3fe0b41e835f52
MHLEE12 <coolmh12@gmail.com>
2024-01-06 22:42:17 +0900

    Study: React Lecture - Node.js 기초
    Node.js 패키지 생성 및 외부 패키지 사용하기

---
+++ Front/React/react_lecture/package-example/index.js
@@ -0,0 +1,9 @@
+const randomColor = require('randomcolor'); // 외부 모듈은 경로를 작성하지 않고 이름을 작성하면 외부 모듈로 인식함
+
+let color1 = randomColor();
+let color2 = randomColor();
+let color3 = randomColor();
+
+console.log(color1);
+console.log(color2);
+console.log(color3);
